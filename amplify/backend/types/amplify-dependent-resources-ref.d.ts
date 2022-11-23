6e29d94f8ffbdd6ebaa2ee7f13495bdda26e5314
CureJuan <iasenzajuancarlos@hotmail.com>
2022-11-23 11:45:36 -0300

    3commit

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++
@@ -1,9 +0,0 @@
-export type AmplifyDependentResourcesAttributes = {
-    "api": {
-        "proyecto3": {
-            "GraphQLAPIKeyOutput": "string",
-            "GraphQLAPIIdOutput": "string",
-            "GraphQLAPIEndpointOutput": "string"
-        }
-    }
-}
