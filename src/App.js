3c27e4b8bb4242de3427f41289016842816fd146
CureJuan <iasenzajuancarlos@hotmail.com>
2022-11-23 10:01:32 -0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,36 @@
+import './App.css';
+//import DiscosCreateForm from './ui-components/DiscosCreateForm'
+
+function App() {
+
+
+  return (
+    <div className="App">
+        <h1 className='title'>
+         Listado de {process.env.REACT_APP_NAME}
+        </h1>
+    
+      <div>
+   
+        <table className="table-list">
+          <thead>
+            <tr>
+              <th>Titulo</th>
+              <th>Banda</th>
+              <th>Año</th>
+            </tr>
+          </thead>
+          <tbody>
+                <tr>
+                   <td>{process.env.name}</td>
+                   <td>{process.env.band}</td>
+                   <td>{process.env.year}</td>
+                </tr>
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
+}
+
+export default App;
